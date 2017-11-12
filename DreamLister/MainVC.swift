d3826876f18f40cca4e67de635d1e6fb4b409fb9
Neato <reggie@users-mbp.attlocal.net>
2017-11-12 08:47:36 -0600

    Cleaned up code and added missing text file

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -222,7 +222,7 @@ extension UIViewController {
         view.addGestureRecognizer(tap)
     }
     
-    func dismissKeyboard() {
+    @objc func dismissKeyboard() {
         view.endEditing(true)
     }
 }
